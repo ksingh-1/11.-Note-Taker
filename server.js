884e25249729a4fa1c7c8f6936ff118e80494861
SSVF2 <kulpreet.s18@gmail.com>
2020-09-15 01:46:38 -0500

    refreshing files

--- server.js
+++ server.js
@@ -3,50 +3,50 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-// port assigned 
+//Port Number 
 const port = 3000;
-// link to main directory
+//Link To Main Directory
 const mainDir = path.join(__dirname, "./Develop/public");
 
-// link to CSS location
+//Link to Css Location
 app.use(express.static('./Develop/public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// pulls notes html file
+//Pull HTML Note File
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(mainDir, "notes.html"));
 });
 
-// link to pull notes json file from directory
+//Path To Pull Notes JSON From Folder
 app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
 });
 
-// determines how the user's input will be outputted into the json file.
+//Gets User Info To Put In Directory
 app.get("/api/notes/:id", function (req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
 });
 
-// pulls index file from main directory
+//Gets Index File From Main Directory
 app.get("*", function (req, res) {
     res.sendFile(path.join(mainDir, "index.html"));
 });
-
+//Posts Notes Stored In Local History
 app.post("/api/notes", function (req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
     let newNote = req.body;
     let uniqueID = (savedNotes.length).toString();
     newNote.id = uniqueID;
     savedNotes.push(newNote);
-
+    //Writes File to page
     fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedNotes));
     console.log("Your notes have been saved to db.json. The content consists of: ", newNote);
     res.json(savedNotes);
 })
-// created backend for deleting notes created.
+//Deletes Notes From Backend History
 app.delete("/api/notes/:id", function (req, res) {
     let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
     let noteID = req.params.id;
@@ -60,11 +60,11 @@ app.delete("/api/notes/:id", function (req, res) {
         currNote.id = newID.toString();
         newID++;
     }
-
+    //Write New Note To Develop File
     fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedNotes));
     res.json(savedNotes);
 })
-
+//App Ready To Use On Certain Port
 app.listen(port, function () {
     console.log(`Now listening to port ${port}. Note Taker App Ready To Be Used!`);
 })
